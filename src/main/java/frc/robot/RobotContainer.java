1130dbfbf510f5a51a804a661177047b9825e2a5
Paul Rensing <prensing@ligerbots.org>
2024-11-10 10:44:52 -0500

    Basic bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,61 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+// import frc.robot.commands.*;
+import frc.robot.subsystems.*;
 
 public class RobotContainer {
+    private static final double JOYSTICK_DEADBAND = 0.05;
+
+    private final CommandXboxController m_driverController = new CommandXboxController(0);
+
+    private final AprilTagVision m_aprilTagVision = new AprilTagVision();
+    private final DriveTrain m_driveTrain = new DriveTrain(m_aprilTagVision);
+
     public RobotContainer() {
         configureBindings();
+        configureAutos();
+
+        m_driveTrain.setDefaultCommand(getDriveCommand());
     }
     
-    private void configureBindings() {}
+    private void configureBindings() {
+        if (Robot.isSimulation()) {
+            DriverStation.silenceJoystickConnectionWarning(true);
+        }
+    }
     
+    private void configureAutos() {
+    }
+
     public Command getAutonomousCommand() {
         return Commands.print("No autonomous command configured");
+    }
+
+    public Command getDriveCommand() {
+        // The controls are for field-oriented driving:
+        // Left stick Y axis -> forward and backwards movement
+        // Left stick X axis -> left and right movement
+        // Right stick X axis -> rotation
+        // note: "rightBumper()"" is a Trigger which is a BooleanSupplier
+        return m_driveTrain.driveCommand( 
+                () -> -conditionAxis(m_driverController.getLeftY()),
+                () -> -conditionAxis(m_driverController.getLeftX()),
+                // if you have a Logitech controller:
+                () -> -conditionAxis(m_driverController.getRawAxis(2)),
+                // () -> -conditionAxis(m_driverController.getRightX()),
+                m_driverController.rightBumper());
+    }
+
+    private double conditionAxis(double value) {
+        value = MathUtil.applyDeadband(value, JOYSTICK_DEADBAND);
+        // Square the axis, retaining the sign
+        return Math.abs(value) * value;
     }
 }
