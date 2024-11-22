50fce21efe8be4ecf1592de7bcbcebd8a9d0316e
evan-gan <139721374+evan-gan@users.noreply.github.com>
2024-11-21 20:36:26 -0500

    Add choreo test (broken right now)

--- src/main/java/frc/robot/commands/SimpleAuto.java
+++ src/main/java/frc/robot/commands/SimpleAuto.java
@@ -4,20 +4,35 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Translation2d;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 
 import frc.robot.subsystems.DriveTrain;
 
-public class SimpleAuto extends SequentialCommandGroup {
 
+public class SimpleAuto extends SequentialCommandGroup {
+    private Pose2d m_initPose;
     /** Creates a new SimpleAuto. */
-    public SimpleAuto(DriveTrain driveTrain) {
+    public SimpleAuto(DriveTrain m_driveTrain) {
+        // try {
+            PathPlannerPath startPath = PathPlannerPath.fromChoreoTrajectory("Test path");
+            m_initPose = startPath.getStartingDifferentialPose();
+            addCommands(m_driveTrain.followPath(startPath));
+
+        // } catch (Exception e) {
+        //     DriverStation.reportError("Unable to load PP path Test", true);
+        //     m_initPose = new Pose2d();
+        // }
         addCommands(
-                new DriveToPosition(driveTrain, new Translation2d(3.0, 0.0)),
-                new DriveToPosition(driveTrain, new Translation2d(0.0, 3.0)),
-                new DriveToPosition(driveTrain, new Translation2d(-3.0, 0.0)),
-                new DriveToPosition(driveTrain, new Translation2d(.0, -3.0))
+                // new DriveToPosition(driveTrain, new Translation2d(3.0, 0.0)),
+                // new DriveToPosition(driveTrain, new Translation2d(0.0, 3.0)),
+                // new DriveToPosition(driveTrain, new Translation2d(-3.0, 0.0)),
+                // new DriveToPosition(driveTrain, new Translation2d(.0, -3.0))
+
         );
     }
 }
