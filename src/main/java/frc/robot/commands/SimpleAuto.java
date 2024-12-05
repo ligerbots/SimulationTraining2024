2089ccde95bd1dd833d3e09af211db78785d0d1b
Paul Rensing <prensing@ligerbots.org>
2024-12-04 20:55:35 -0500

    fix SimpleAuto to use the DriveTrain path loading

--- src/main/java/frc/robot/commands/SimpleAuto.java
+++ src/main/java/frc/robot/commands/SimpleAuto.java
@@ -5,37 +5,30 @@
 package frc.robot.commands;
 
 import com.pathplanner.lib.path.PathPlannerPath;
-
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
 
 import frc.robot.subsystems.DriveTrain;
 
-
 public class SimpleAuto extends SequentialCommandGroup {
-    // private Pose2d m_initPose;
+    private Pose2d m_initPose;
+
     /** Creates a new SimpleAuto. */
-    public SimpleAuto(DriveTrain m_driveTrain) {
-        // try {
-            //PathPlannerPath C1 to shoot
-            //ChoreoPath C1 to shoot
-            // PathPlannerPath startPath = PathPlannerPath.fromChoreoTrajectory("ChoreoPath C1 to shoot");
-            PathPlannerPath startPath = PathPlannerPath.fromPathFile("PathPlannerPath C1 to shoot");
-            // m_initPose = startPath.getStartingDifferentialPose();
-            addCommands(m_driveTrain.followPath(startPath));
+    public SimpleAuto(DriveTrain driveTrain) {
 
-        // } catch (Exception e) {
-        //     DriverStation.reportError("Unable to load PP path Test", true);
-        //     m_initPose = new Pose2d();
-        // }
-        addCommands(
-                // new DriveToPosition(driveTrain, new Translation2d(3.0, 0.0)),
-                // new DriveToPosition(driveTrain, new Translation2d(0.0, 3.0)),
-                // new DriveToPosition(driveTrain, new Translation2d(-3.0, 0.0)),
-                // new DriveToPosition(driveTrain, new Translation2d(.0, -3.0))
+        // PathPlannerPath C1 to shoot
+        // ChoreoPath C1 to shoot
+        // PathPlannerPath startPath = PathPlannerPath.fromChoreoTrajectory("ChoreoPath C1 to shoot");
 
-        );
+        PathPlannerPath startPath = DriveTrain.loadPath("PathPlannerPath C1 to shoot");
+        m_initPose = startPath.getStartingDifferentialPose();
+        addCommands(driveTrain.followPath(startPath));
+        
+        // addCommands(
+        //         new DriveToPosition(driveTrain, new Translation2d(3.0, 0.0)),
+        //         new DriveToPosition(driveTrain, new Translation2d(0.0, 3.0)),
+        //         new DriveToPosition(driveTrain, new Translation2d(-3.0, 0.0)),
+        //         new DriveToPosition(driveTrain, new Translation2d(.0, -3.0))
+        // );
     }
 }
