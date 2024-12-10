5df91dda5df5495b086e2b8a67bd0d68750f5d7f
evan-gan <139721374+evan-gan@users.noreply.github.com>
2024-12-09 20:12:48 -0500

    Got pathplanner working in simulation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,7 +20,7 @@ public class RobotContainer {
     // Need to know where the robot starts on the field
     // For this simulation, just make it fixed
     // Location: x = 2 meters, y = 1/2 of field, angle is forward
-    private static final Pose2d START_LOCATION = new Pose2d(8.03, 0.77, Rotation2d.fromDegrees(163.55));
+    private static final Pose2d START_LOCATION = new Pose2d(8.03, 0.77, Rotation2d.fromDegrees(0.46));
 
     private final CommandXboxController m_driverController = new CommandXboxController(0);
 
